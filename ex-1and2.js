76fca599e871e4a2be8cb62f7aed433e11510a0a
Kanompang <suparadajk21@gmail.com>
2024-08-31 19:36:00 +0700

    BP5-Kanompang

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,18 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let Name = 'Karter';
+let Surname = 'Anderson';
+let Address = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai';
+console.log(Name);
+console.log(Surname);
+console.log(Address);
+const RestaurantName = "Chef's Table by Chef James";
+const NetPrice = "15000";
+const OpenedRestaurant ="1500";
+console.log(RestaurantName);
+console.log(NetPrice);
+console.log(OpenedRestaurant);
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641';
+console.log(Address);
